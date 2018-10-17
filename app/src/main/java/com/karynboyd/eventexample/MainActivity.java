9564b311e9116b1ab6c01ca16970add6614c86f2
Karyn Boyd <karynboyd74@gmail.com>
2018-10-17 14:38:43 -0500

    Chapter 24 code along complete and working

--- app/src/main/java/com/karynboyd/eventexample/MainActivity.java
+++ app/src/main/java/com/karynboyd/eventexample/MainActivity.java
@@ -2,12 +2,40 @@ package com.karynboyd.eventexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.buttonClick);
+            button.setOnClickListener(
+                    new Button.OnClickListener() {
+                        public void onClick(View v) {
+                            TextView statusText = (TextView)findViewById(R.id.status);
+
+                            if(statusText.getText() == "Button clicked")
+                                statusText.setText("Do it again!");
+                            else
+                            statusText.setText("Button clicked");
+                        }
+                    }
+            );
+
+            button.setOnLongClickListener(
+                    new Button.OnLongClickListener() {
+                        public boolean onLongClick(View v) {
+                            TextView statusText = (TextView)findViewById(R.id.status);
+
+                            statusText.setText("Long button click");
+
+                            return false;
+                        }
+                    }
+            );
     }
 }
